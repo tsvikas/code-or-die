8db7604ce54b9fa561fe81406022dd708e8006ce
tsvikas <tsvikas@gmail.com>
2018-07-02 22:20:59 +0300

    server: changed cheroot to waitress

--- server.py
+++ server.py
@@ -1,20 +1,14 @@
 import logging
 import sys
 
-from cheroot.wsgi import Server as WSGIServer, PathInfoDispatcher
+import waitress
 from pymongo import MongoClient
 
 from app import get_app
 
-logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     db = MongoClient('localhost', 27017)['code-or-die']
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    app_server = PathInfoDispatcher({'/': get_app(db)})
-    server = WSGIServer(('0.0.0.0', 5000), app_server, numthreads=150)
-    try:
-        logger.info('starting server!')
-        server.start()
-    except KeyboardInterrupt:
-        server.stop()
+    app = get_app(db)
+    waitress.serve(app, host='0.0.0.0', port=5000, threads=150)
